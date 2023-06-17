5295d2d420f4ddfe46844e1f648117f0a3b25527
as3545 <86479682+as3545@users.noreply.github.com>
2023-06-17 13:05:02 +0530

    Pattern 3.cpp

--- Pattern 3.cpp
+++ Pattern 3.cpp
@@ -5,7 +5,7 @@ Problem statement
 Geek is very fond of patterns. Once, his teacher gave him a  pattern to solve. He gave Geek an integer n and asked him to build a pattern.
 Help Geek to build a pattern.
 
- 
+
 
 Example 1:
 
