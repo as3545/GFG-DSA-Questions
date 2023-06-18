80938f26d2a3d28d79db25717104facfc9d18298
as3545 <86479682+as3545@users.noreply.github.com>
2023-06-18 16:42:38 +0530

    Odd Divisors.cpp

---
+++ Odd Divisors.cpp
@@ -0,0 +1,47 @@
+/*
+Problem Statement
+-----------------
+Given a natural number N, find the count of numbers from 1 to N that have an odd number of divisors. 
+
+ 
+
+Example 1:
+
+Input:
+N = 1
+Output:
+1
+Explanation:
+1 has only one 
+divisor {1}.
+ 
+
+Example 2:
+
+Input:
+N = 4
+Output:
+2
+Explanation:
+4 has an odd number 
+of divisors {1, 2, 4}.
+ 
+
+Your Task:
+
+You don't need to read input or print anything. Your task is to complete the function oddNumberOfDivisor() which takes an integer N and returns the count of numbers from 1 to n that have an odd number of divisors. 
+
+ 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+ 
+
+Constraints:
+1 <= N <= 106
+
+*/
+// Code:
+
+return sqrt(N);
