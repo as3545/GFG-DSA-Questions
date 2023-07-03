c808d8f8e54a209b78340c094265739718439211
as3545 <86479682+as3545@users.noreply.github.com>
2023-07-03 12:47:52 +0530

    Output Window.cpp

---
+++ output Window.cpp
@@ -0,0 +1,52 @@
+/*
+
+Given a number N, find the Nth term in the series 1, 3, 6, 10, 15, 21…
+
+ 
+
+Example 1:
+
+Input :
+N = 4 
+Output:
+10
+Explanation:
+The 4th term of the Series is 10.
+Example 2:
+
+Input :
+N = 3 
+Output:
+6
+Explanation:
+The 3rd term of the Series is 6.
+ 
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function findNthTerm() which takes an Integer N as input and returns the answer.
+
+ 
+
+Expected Time Complexity: O(1)
+Expected Auxiliary Space: O(1)
+
+ 
+
+Constraints:
+1 <= N <= 104
+
+*/
+//Code:
+
+ public:
+    int findNthTerm(int N) {
+        // code here
+        int t=1;
+        if(N==1){
+            return t;
+        }
+        for(int i=2;i<=N;i++){
+            t=t+i;
+        } return t;
+    }
+};
