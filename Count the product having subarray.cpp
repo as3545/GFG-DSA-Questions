e70969ff5d226928d6cc2f148569804f2f5ab166
as3545 <86479682+as3545@users.noreply.github.com>
2023-07-04 14:34:52 +0530

    Count the product having subarray.cpp

---
+++ Count the product having subarray.cpp
@@ -0,0 +1,55 @@
+/*
+Given an array of positive numbers, the task is to find the number of possible contiguous subarrays having product less than a given number k.
+
+Example 1:
+
+Input : 
+n = 4, k = 10
+a[] = {1, 2, 3, 4}
+Output : 
+7
+Explanation:
+The contiguous subarrays are {1}, {2}, {3}, {4} 
+{1, 2}, {1, 2, 3} and {2, 3} whose count is 7.
+Example 2:
+
+Input:
+n = 7 , k = 100
+a[] = {1, 9, 2, 8, 6, 4, 3}
+Output:
+16
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function countSubArrayProductLessThanK() which takes the array a[], its size n and an integer k as inputs and returns the count of required subarrays.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1<=n<=106
+1<=k<=1015
+1<=a[i]<=105
+*/
+
+//Code:
+class Solution{
+  public:
+    int countSubArrayProductLessThanK(const vector<int>& a, int n, long long k) {
+        long long prod = 1;
+        
+        int ans = 0;
+        int s = 0,e = 0;
+        
+        for(;e < n;e++){
+            prod *= a[e];
+            while(s < e and prod >= k){
+                prod /= a[s];
+                s++;
+            }
+            if(prod < k)
+                ans += (e-s+1);
+        }
+        
+        
+        return ans;
+    }
+};
