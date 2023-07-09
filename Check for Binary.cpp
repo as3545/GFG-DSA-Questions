e4fb03fbbd69aadbf0c5be5c4ca5c2f6ed69d59b
as3545 <86479682+as3545@users.noreply.github.com>
2023-07-10 00:53:39 +0530

    Check for Binary.cpp

---
+++ Check for Binary.cpp
@@ -0,0 +1,15 @@
+/*
+  
+*/
+//Code
+
+bool isBinary(string s)
+{
+   // Your code here
+    for(int i=0;i<s.length();i++){
+        if(s[i]>'1'){
+        return false;
+    }
+    
+} return true;
+}
