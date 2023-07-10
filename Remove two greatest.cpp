9ca903265d561c30d01b2162c92236a62d772ec6
as3545 <86479682+as3545@users.noreply.github.com>
2023-07-11 00:39:02 +0530

    Remove two greatest.cpp

---
+++ Remove two greatest.cpp
@@ -0,0 +1,45 @@
+/*
+Given an array of N distinct elements, the task is to find all elements in array except two greatest elements in sorted order.
+
+
+Example 1:
+
+Input : 
+a[] = {2, 8, 7, 1, 5}
+Output :
+1 2 5 
+Explanation :
+The output three elements have two or
+more greater elements.   
+Example 2:
+
+Input :
+a[] = {7, -2, 3, 4, 9, -1}
+Output :
+-2 -1 3 4
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function findElements() which takes the array A[] and its size N as inputs and return the vector sorted values denoting the elements in array which have at-least two greater elements than themselves.
+
+Expected Time Complexity: O(N*Log(N))
+Expected Auxiliary Space: O(N)
+
+Constraints:
+3 ≤ N ≤ 105
+-106 ≤ Ai ≤ 106
+
+*/
+
+
+//Code
+public:
+    vector<int> findElements(int a[], int n)
+    { vector<int>v;
+    int ans;
+    sort(a, a+n);
+    for(int i=0;i<n-2;i++){
+        v.push_back(a[i]);
+    } return v;
+        // Your code goes here   
+    }
+};
