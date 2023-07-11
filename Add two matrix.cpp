c92ac1539f618aa1fd4fa590223a832f8fd6fd51
as3545 <86479682+as3545@users.noreply.github.com>
2023-07-12 01:18:17 +0530

    Add two matrix.cpp

---
+++ Add two matrix.cpp
@@ -0,0 +1,45 @@
+/*
+Given two square matrices matrixA and matrixB of size n x n.  Find the addition of these two matrices.
+
+Note :- You don't have to return anything in this question (Refer YourTask section).
+ 
+
+Example 1:
+
+Input: matrixA = {{1, 2}, {3, 4}},
+matrixB = {{4, 3}, {2, 1}}
+Output: {{5, 5}, {5, 5}}
+Example 1:
+
+Input: matrixA = {{2, 2}, {2, 2}},
+matrixB = {{3, 3}, {3, 3}}
+Output: {{5, 5},{5, 5}}
+ 
+
+Your Task:
+You don't need to read or print anything. Your task is to complete the function Addition() which takes matrixA and matrixB as input parameters and adds two matrices. Do the addition without using extra memory. The output matrix must be in matrixA.
+ 
+
+Expected Time Complexity: O(n2)
+Expected Space Complexity: O(1)
+ 
+
+Constraints:
+1 <= n <= 100
+*/
+//Code
+//User function Template for C++
+
+class Solution {
+public:
+    void Addition(vector<vector<int> >& matrixA, vector<vector<int>>&matrixB) {
+        // Code here
+        int n=matrixA.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                matrixA[i][j]+=matrixB[i][j];
+            }
+        }
+        
+    }
+};
