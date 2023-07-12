fe858e288e5bf034e76a43d491070da0f396d2f5
as3545 <86479682+as3545@users.noreply.github.com>
2023-07-12 21:01:41 +0530

    Search an Element in an array.cpp

---
+++ Search an Element in an array.cpp
@@ -0,0 +1,19 @@
+/*
+Search an Element in an array
+
+*/
+
+//Code
+int search(int arr[], int N, int X)
+    {
+        
+        // Your code here
+        for(int i=0;i<N;i++){
+            if(arr[i]==X){
+                return i;
+            }  
+        } return -1;
+        
+    }
+
+};
