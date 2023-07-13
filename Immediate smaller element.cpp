9fadfe2627715e67fce4cf31c5f9cbd91668f47f
as3545 <86479682+as3545@users.noreply.github.com>
2023-07-13 14:53:43 +0530

    Immediate smaller element.cpp

--- Immediate smaller element.cpp
+++ Immediate smaller element.cpp
@@ -47,11 +47,12 @@ Constraints:
 
 //Code
 
-	    for(int i=0;i<n;i++){
-	        if(arr[i]<arr[i+1])
-	            arr[i]=-1;
+	   for(int i=0;i<n;i++){
+	        if(arr[i]>arr[i+1])
+	            arr[i]=arr[i+1];
 	         else {
-	             arr[i]=arr[i+1];
+	            arr[i]=-1;
+	             
 	         } 
 	            
 	    } arr[n-1]=-1;
