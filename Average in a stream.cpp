2104fe857df81a7c274b03cdf3347bdf4da08f1c
as3545 <86479682+as3545@users.noreply.github.com>
2023-07-30 19:25:46 +0530

    Update Average in a stream.cpp

--- Average in a stream.cpp
+++ Average in a stream.cpp
@@ -7,14 +7,15 @@
 class Solution{
 public:	
 	vector<float> streamAvg(int arr[], int n) {
-	   vector<float> avgStream;
+	   vector<float> v;
 	    double avg=0,sum=0;
 	    
 	    for(int i=0;i<n;i++){
 	        sum+=arr[i];
 	        avg=sum/(i+1);
 	        setprecision(2);
-	        avgStream.push_back(avg);
+	        v.push_back(avg);
+		    
 	    }
 	    
 	    return avgStream;
