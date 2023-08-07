d9d4058154e78f4577dff09394e0de46d83bb834
as3545 <86479682+as3545@users.noreply.github.com>
2023-08-07 10:05:53 +0530

    Data type.cpp

---
+++ Data type.cpp
@@ -0,0 +1,50 @@
+/*
+
+Read a value and store it in the appropriate C++ Data Type. 
+
+Example 1:
+
+Input: 
+2 h 2.555
+Output:
+2
+h
+2.555 
+Explanation:
+The three inputs are printed in order.
+ 
+
+Your Task:
+Your task is to complete each of the given functions 
+cppIntType() : read an integer input, store it in appropriate data type and return it. 
+cppCharType() : read a character input, store it in appropriate data type and return it.  
+cppFloatType() : read a float input, store it in appropriate data type and return it. 
+
+
+Expected Time Complexity: O(1)
+Expected Auxiliary Space: O(1)
+
+  */
+
+
+
+//Code
+ int n;
+        cin>>n;
+        
+        return n;
+    }
+    
+    char cppCharType() {
+        // code here
+        char c;
+        cin>>c;
+        return c;
+    
+    }
+    
+    float cppFloatType() {
+        // code here
+        float f;
+        cin>>f;
+        return f;
