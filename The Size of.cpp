f1800099f0f512d8fb5461852860e63bfbf6c762
as3545 <86479682+as3545@users.noreply.github.com>
2023-08-07 09:52:11 +0530

    The Size of.cpp

---
+++ The Size of.cpp
@@ -0,0 +1,11 @@
+/*
+
+*/
+//Code
+
+ a=b/c;
+    b=b/a;
+    c=c/a;
+    l=b+1;
+    cout<<sizeof(a)<<" "<<sizeof(c)<<" "<<sizeof(b)<<" "<<sizeof(l)<<endl;
+    cout<<sizeof(d)<<" "<<sizeof(d[3])<<endl;
