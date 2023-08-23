63ac571a33480427d02e8b1c28419741ee095248
as3545 <86479682+as3545@users.noreply.github.com>
2023-08-23 16:54:28 +0530

    Pattern 14.cpp

---
+++ Pattern 14.cpp
@@ -0,0 +1,13 @@
+//Code
+
+ void printTriangle(int n) {
+        // code here
+        
+        for(int i=0;i<n;i++){
+            char c='A';
+                for(int j=0;j<=i;j++){
+                cout<<c;
+                c++;
+            } cout<<endl;
+        }
+    }
