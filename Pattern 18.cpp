ef84ac625e4a94dd3f28a6d3555f97a6724ca32e
as3545 <86479682+as3545@users.noreply.github.com>
2023-08-23 17:51:56 +0530

    Pattern 18.cpp

---
+++ Pattern 18.cpp
@@ -0,0 +1,50 @@
+/*
+
+  
+  Ram is very fond of patterns. Once, his teacher gave him a pattern to solve. He gave Ram an integer n and asked him to build a pattern.
+
+Help Ram build a pattern.
+
+ 
+
+Example 1:
+
+Input: 5
+
+Output:
+E
+E D
+E D C
+E D C B
+E D C B A
+ 
+
+Example 2:
+
+Input: 3
+
+Output:
+C
+C B
+C B A
+Your Task:
+
+You don't need to input anything. Complete the function printTriangle() which takes an integer n  as the input parameter and print the pattern.
+
+Constraints:
+
+1<= N <= 20
+
+  */
+
+//Code
+public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=0;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<char('A'+n-j)<<" ";
+                
+            } cout<<endl;
+        }
+    }
