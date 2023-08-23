8578ca25045fc808e941eebe668d0be6a791b451
as3545 <86479682+as3545@users.noreply.github.com>
2023-08-23 16:47:50 +0530

    Pattern  5.cpp

--- Pattern  5.cpp
+++ Pattern  5.cpp
@@ -2,7 +2,7 @@
 Problem Statement:
 -----------------
 Geek is very fond of patterns. Once, his teacher gave him a pattern to solve. He gave Geek an integer n and asked him to build a pattern.
-
+ 
 Help Geek build a star pattern.
 
  
