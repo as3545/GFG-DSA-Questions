89818a581e3d78dea492b3d62401c28616d46d0e
as3545 <86479682+as3545@users.noreply.github.com>
2023-09-02 19:05:51 +0530

    Height of Heap.cpp

---
+++ Height of Heap.cpp
@@ -0,0 +1,56 @@
+/*
+
+Given a Binary Heap of size N in an array arr[]. Write a program to calculate the height of the Heap.
+
+Note: Return 1 if the N is 1.
+
+Example 1:
+
+Input: N = 6
+arr = {1, 3, 6, 5, 9, 8}
+Output: 2
+Explaination: The tree is like the following
+           (1)
+       /   \
+    (3)    (6)
+    / \     /
+  (5) (9) (8)
+Example 2:
+
+Input: N = 9
+arr = {3, 6, 9, 2, 15, 10, 14, 5, 12}
+Output: 3
+Explaination: The tree looks like following
+               (2)
+        /      \
+      (3)      (9)
+     /  \     /   \
+   (5) (15) (10) (14)
+   / \
+ (6) (12)
+Your Task:
+You do not need to read input or print anything. Your task is to complete the function heapHeight() which takes the value N and the array arr as input parameters and returns the height of the heap.
+
+Expected Time Complexity: O(logN)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ N ≤ 104
+1 ≤ arr[i] ≤ 106
+
+
+
+*/
+
+//Code
+
+int heapHeight(int N, int arr[]){
+        int count = 0;
+        while(N > 0)
+        {
+            N /= 2;
+            count++;
+        }
+        return count-1;
+    }
+};
