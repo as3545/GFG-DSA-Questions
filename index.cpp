1740a7a6e25de4235f1e492c6576cb5bfe84cbc5
as3545 <86479682+as3545@users.noreply.github.com>
2024-06-16 17:58:54 +0530

    index.cpp

--- index.cpp
+++ index.cpp
@@ -13,3 +13,34 @@ vector<int> subarraySum(vector<int>arr, int n, long long s)
             }
         } return {-1};
     }
+
+
+int start = 0, last = 0;
+        long long cursum = 0;
+        vector<int> res;
+        bool flag = false;
+       for (int i = 0; i < n; i++) {
+            cursum += arr[i];
+            
+            if (cursum >= s) {
+                last = i;
+                
+                while (s < cursum && start < last) {
+                    cursum -= arr[start];
+                    ++start;
+                }
+                
+                if (cursum == s) {
+                    res.push_back(start + 1);
+                    res.push_back(last + 1);
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        
+        if (flag == false) {
+            res.push_back(-1);
+        }
+        
+        return res;
